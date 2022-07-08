1f338ff048c0a565b4fd6cb59c2ae9b609ec7d40
NexusRule <fxxsj1@gmail.com>
2022-07-07 23:57:30 -0500

    hh

--- src/lab11.ino
+++ src/lab11.ino
@@ -1,38 +1,59 @@
+SYSTEM_MODE(MANUAL)
+SYSTEM_THREAD(ENABLED)
+
 #include "oled-wing-adafruit.h"
 #include "LIS3DH.h"
-SYSTEM_THREAD(ENABLED);
-SYSTEM_MODE(MANUAL);
+
 OledWingAdafruit display;
-
+LIS3DHSPI accel(SPI, D3, WKP);
 const unsigned long PRINT_SAMPLE_PERIOD = 100;
-
-LIS3DHSPI accel(SPI, A2);
+unsigned long lastPrintSample = 0;
 
 void setup()
 {
-	Serial.begin(9600);
-
 	display.setup();
-    display.clearDisplay();
-
+	display.clearDisplay();
+	display.setCursor(0, 0);
+	display.setTextSize(1);
+	display.setTextSize(1);
+	display.setTextColor(WHITE);
+	display.println("DISPLAY CONNECTED");
+	display.display();
+	delay(2000);
 	LIS3DHConfig config;
 	config.setAccelMode(LIS3DH::RATE_100_HZ);
+	accel.setup(config);
 }
 
 void loop()
 {
 	display.loop();
-	LIS3DHSample sample;
-
-	if (accel.getSample(sample))
+	if (millis() - lastPrintSample >= PRINT_SAMPLE_PERIOD)
 	{
+		lastPrintSample = millis();
 
-		display.clearDisplay();
-		display.setTextSize(1);
-		display.setTextColor(WHITE);
-		display.setCursor(0, 0);
-		display.printlnf("%d,%d,%d", sample.x, sample.y, sample.z);
-		display.display();
-		delay(200);
+		LIS3DHSample sample;
+		if (accel.getSample(sample))
+		{
+			display.clearDisplay();
+			display.setCursor(0, 0);
+			display.setTextSize(1);
+			display.print(sample.x);
+			display.println(" - X");
+			display.print(sample.y);
+			display.println(" - Y");
+			display.print(sample.z);
+			display.println(" - Z");
+			display.display();
+			delay(200);
+		}
+		else
+		{
+			display.clearDisplay();
+			display.setCursor(0, 0);
+			display.setTextSize(1);
+			display.println("no sample");
+			display.display();
+		}
 	}
 }
